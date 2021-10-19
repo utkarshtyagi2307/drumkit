1a58e13c5e67cc0fa85b874c371e3f21906090b3
utkarshtyagi2307 <83960027+utkarshtyagi2307@users.noreply.github.com>
2021-10-19 23:12:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+
+ function keypressed(key){           
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+    
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+    
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+    
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+    
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+    
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+    
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+    
+        default:
+            break;
+    }
+}
+
+
+for (var i = 0; i<document.querySelectorAll("button.drum").length; i++) {
+    document.querySelectorAll("button.drum")[i].addEventListener("click", function(){
+      
+        var buttonPress = this.innerHTML;
+        keypressed(buttonPress);
+
+        buttonAnimation(buttonPress);
+        
+    });
+}
+
+
+document.addEventListener("keypress", function(event){
+    var keyboardKey = event.key;
+    keypressed(keyboardKey);
+
+    buttonAnimation(keyboardKey);
+});
+
+
+
+function buttonAnimation(keyPress) {
+
+    var activeButton = document.querySelector("."+keyPress);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    }, 100)
+    
+}
+
